0ea3a06d0d044dc74d0792b4a1e8ba0230afc865
Max Kutovoy <maxkutovoy@yandex.ru>
2021-12-07 17:50:33 +0600

    Отформатироваал код

--- scripts.py
+++ scripts.py
@@ -2,8 +2,14 @@ import random
 
 from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
 
-from datacenter.models import (Chastisement, Commendation, Lesson, Mark,
-                               Schoolkid, Subject)
+from datacenter.models import (
+    Chastisement,
+    Commendation,
+    Lesson,
+    Mark,
+    Schoolkid,
+    Subject,
+)
 
 
 def fix_marks(schoolkid):
@@ -15,11 +21,11 @@ def fix_marks(schoolkid):
                 bad_mark.points = random.randint(4, 5)
                 bad_mark.save()
         else:
-            print(f'Плохих оценок у ученика {kid} не найдено')
+            print(f"Плохих оценок у ученика {kid} не найдено")
     except Schoolkid.ObjectDoesNotExist:
-        print('Ученик не найден, проверьте правильность запроса')
+        print("Ученик не найден, проверьте правильность запроса")
     except Schoolkid.MultipleObjectsReturned:
-        print('Слишком много вариантов, уточните запрос')
+        print("Слишком много вариантов, уточните запрос")
 
 
 def remove_chastisements(schoolkid):
@@ -29,62 +35,75 @@ def remove_chastisements(schoolkid):
         if chastisements:
             chastisements.delete()
         else:
-            print(f'Замечаний для ученика {kid} не найдено')
+            print(f"Замечаний для ученика {kid} не найдено")
     except Schoolkid.ObjectDoesNotExist:
-        print('Ученик не найден, проверьте правильность запроса')
+        print("Ученик не найден, проверьте правильность запроса")
     except Schoolkid.MultipleObjectsReturned:
-        print('Слишком много вариантов, уточните запрос')
+        print("Слишком много вариантов, уточните запрос")
 
 
 def create_commendation(schoolkid, subject):
     commendations = [
-        'Молодец!',
-        'Отлично!',
-        'Хорошо!',
-        'Гораздо лучше, чем я ожидал!',
-        'Ты меня приятно удивил!',
-        'Великолепно!',
-        'Прекрасно!',
-        'Ты меня очень обрадовал!',
-        'Именно этого я давно ждал от тебя!',
-        'Сказано здорово – просто и ясно!',
-        'Ты, как всегда, точен!',
-        'Очень хороший ответ!',
-        'Талантливо!',
-        'Ты сегодня прыгнул выше головы!',
-        'Я поражен!',
-        'Уже существенно лучше!',
-        'Потрясающе!',
-        'Замечательно!',
-        'Прекрасное начало!',
-        'Так держать!',
-        'Ты на верном пути!',
-        'Здорово!',
-        'Это как раз то, что нужно!',
-        'Я тобой горжусь!',
-        'С каждым разом у тебя получается всё лучше!',
-        'Мы с тобой не зря поработали!',
-        'Я вижу, как ты стараешься!',
-        'Ты растешь над собой!',
-        'Ты многое сделал, я это вижу!',
-        'Теперь у тебя точно все получится!',
+        "Молодец!",
+        "Отлично!",
+        "Хорошо!",
+        "Гораздо лучше, чем я ожидал!",
+        "Ты меня приятно удивил!",
+        "Великолепно!",
+        "Прекрасно!",
+        "Ты меня очень обрадовал!",
+        "Именно этого я давно ждал от тебя!",
+        "Сказано здорово – просто и ясно!",
+        "Ты, как всегда, точен!",
+        "Очень хороший ответ!",
+        "Талантливо!",
+        "Ты сегодня прыгнул выше головы!",
+        "Я поражен!",
+        "Уже существенно лучше!",
+        "Потрясающе!",
+        "Замечательно!",
+        "Прекрасное начало!",
+        "Так держать!",
+        "Ты на верном пути!",
+        "Здорово!",
+        "Это как раз то, что нужно!",
+        "Я тобой горжусь!",
+        "С каждым разом у тебя получается всё лучше!",
+        "Мы с тобой не зря поработали!",
+        "Я вижу, как ты стараешься!",
+        "Ты растешь над собой!",
+        "Ты многое сделал, я это вижу!",
+        "Теперь у тебя точно все получится!",
     ]
     try:
         kid = Schoolkid.objects.get(full_name__contains=schoolkid)
-        lessons = Lesson.objects.filter(year_of_study='6', group_letter='А', subject__title=subject).order_by('-date')
+        lessons = Lesson.objects.filter(
+            year_of_study=kid.yeayear_of_study,
+            group_letter=kid.group_letter,
+            subject__title=subject,
+        ).order_by("-date")
         if not lessons:
             raise Lesson.ObjectDoesNotExist
         for lesson in lessons:
-            commendation = Commendation.objects.filter(created=lesson.date, schoolkid=kid, subject=lesson.subject,
-                                                       teacher=lesson.teacher)
+            commendation = Commendation.objects.filter(
+                created=lesson.date,
+                schoolkid=kid,
+                subject=lesson.subject,
+                teacher=lesson.teacher,
+            )
             if not commendation:
                 random_commendation = random.choice(commendations)
-                Commendation.objects.create(text=random_commendation, created=lesson.date, schoolkid=kid,
-                                            subject=lesson.subject, teacher=lesson.teacher)
+                Commendation.objects.create(
+                    text=random_commendation,
+                    created=lesson.date,
+                    schoolkid=kid,
+                    subject=lesson.subject,
+                    teacher=lesson.teacher,
+                )
                 break
     except Schoolkid.ObjectDoesNotExist:
-        print('Ученик не найден, проверьте правильность запроса')
+        print("Ученик не найден, проверьте правильность запроса")
     except Schoolkid.MultipleObjectsReturned:
-        print('Слишком много вариантов, уточните запрос')
+        print("Слишком много вариантов, уточните запрос")
     except Lesson.ObjectDoesNotExist:
-        print('Предмет не найден, проверьте правильность запроса')
+        print("Предмет не найден, проверьте правильность запроса")
